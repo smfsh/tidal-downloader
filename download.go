92fc53def647a44c5afe574253d9a31ed8375475
Miles McLean <Mills00013@gmail.com>
2019-08-29 16:27:47 -0500

    Added file downloading

--- download.go
+++ download.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 )
 
-func get(url string, c *tidalConfig) map[string]interface{} {
+type tidalObject struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Duration    int    `json:"duration"`
+	TrackNumber int    `json:"trackNumber"`
+	Url         string `json:"url"`
+}
+
+func get(url string, c *tidalConfig) tidalObject {
 	var sid string
 
 	if c.quality != "LOSSLESS" {
@@ -25,6 +35,7 @@ func get(url string, c *tidalConfig) map[string]interface{} {
 	q := req.URL.Query()
 	q.Add("sessionId", sid)
 	q.Add("countryCode", "US")
+	q.Add("soundQuality", c.quality)
 	req.URL.RawQuery = q.Encode()
 
 	client := &http.Client{}
@@ -40,7 +51,7 @@ func get(url string, c *tidalConfig) map[string]interface{} {
 	}
 	//fmt.Printf("%s\n", body)
 
-	jsonMap := make(map[string]interface{})
+	var jsonMap tidalObject
 	err = json.Unmarshal(body, &jsonMap)
 	if err != nil {
 		panic(err)
@@ -50,16 +61,40 @@ func get(url string, c *tidalConfig) map[string]interface{} {
 }
 
 // Track: 62437814
-func getTrack(id int, c *tidalConfig) map[string]interface{} {
+func getTrack(id int, c *tidalConfig) tidalObject {
 	return get("tracks/"+strconv.Itoa(id), c)
 }
 
 // Album: 62437813
-func getAlbum(id int, c *tidalConfig) map[string]interface{} {
+func getAlbum(id int, c *tidalConfig) tidalObject {
 	return get("albums/"+strconv.Itoa(id), c)
 }
 
 // Artist: 5221673
-func getArtist(id int, c *tidalConfig) map[string]interface{} {
+func getArtist(id int, c *tidalConfig) tidalObject {
 	return get("artists/"+strconv.Itoa(id), c)
+}
+
+func getStreamUrl(id int, c *tidalConfig) tidalObject {
+	return get("tracks/"+strconv.Itoa(id)+"/streamUrl", c)
+}
+
+func downloadFile(url string, output string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
--- tidal.go
+++ tidal.go
@@ -10,33 +10,31 @@ func main() {
 	fmt.Println("Starting Tidal Downloader")
 
 	// Prepare default configuration.
-	c := newTidalConfig("LOSSLESS", "username", "password")
+	c := newTidalConfig("HI_RES", "username", "password")
 
 	// Login with configuration.
 	login(c)
 
-	track := getTrack(62437814, c)
+	track := getTrack(80185439, c)
 	//get("albums/62437813", c)
-	album := getAlbum(62437813, c)
+	album := getAlbum(80185437, c)
 
 	//printMap("", track)
 	//printMap("", album)
 
 	fmt.Println("Preparing download:")
-	fmt.Println("Album Title   ", album["title"])
-	fmt.Println("Track Title   ", track["title"])
-	fmt.Println("Duration      ", track["duration"])
-	fmt.Println("Track Number  ", track["trackNumber"])
-}
+	fmt.Println("Album Title   ", album.Title)
+	fmt.Println("Track Title   ", track.Title)
+	fmt.Println("Track ID      ", track.Id)
+	fmt.Println("Duration      ", track.Duration)
+	fmt.Println("Track Number  ", track.TrackNumber)
 
-func printMap(space string, m map[string]interface{}) {
-	for k, v := range m {
-		if mv, ok := v.(map[string]interface{}); ok {
-			fmt.Printf("{ \"%v\": \n", k)
-			printMap(space+"\t", mv)
-			fmt.Printf("}\n")
-		} else {
-			fmt.Printf("%v %v : %v\n", space, k, v)
-		}
+	//var id int = track["id"]
+	stream := getStreamUrl(track.Id, c)
+	//fmt.Println(stream.Url)
+
+	err := downloadFile(stream.Url, "file.flac")
+	if err != nil {
+		panic(err)
 	}
 }
